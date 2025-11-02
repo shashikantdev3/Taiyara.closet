3603870aa5ae4b551998c1ee4d7c8778fd866754
Shashikant <imshashikantdev@gmail.com>
2025-11-03 01:50:37 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 
-// ✅ Make sure the base path matches your GitHub repo name
 export default defineConfig({
   plugins: [vue()],
-  base: './', // <-- required for GitHub Pages
+  base: '/Taiyara.closet/', 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src'
     }
   }
 })
